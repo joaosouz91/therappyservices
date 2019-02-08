b92d3b6c2279d1e845d99ba51636442bf956ffac
Joao Victor <joaovictor.souz@gmail.com>
2019-02-08 13:22:30 -0200

    modify db connection string parameters

--- server.js
+++ server.js
@@ -6,10 +6,17 @@ var express = require('express'),
     bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/msgdb');
+
+//mongoose.connect('mongodb://localhost/msgdb');
+mongoose.connect('mongodb://heroku_rq4kkkp3:san46480@ds229722.mlab.com:29722/heroku_rq4kkkp3');
+
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(bodyParser.json());
+
 var routes = require('./api/routes/msgRoutes');
+
 routes(app);
+
 app.listen(port);
 console.log('Message RESTful API server started on: ' + port);
