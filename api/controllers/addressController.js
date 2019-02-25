b73c6fc0b81119ba75bd7048111cac24eeea2a71
Joao Victor <joaovictor.souz@gmail.com>
2019-02-25 02:24:46 -0300

    segundo servico

--- api/controllers/addressController.js
+++ api/controllers/addressController.js
@@ -1,41 +1,79 @@
 'use strict';
 
 var request = require('request');
-var bodyParser = require('body-parser');
 
 exports.get_latitude_longitude = function (req, res) {
 
     var addressReq = req.body.address;
-    var url =  'https://maps.googleapis.com/maps/api/geocode/json?&address='
-                + encodeURIComponent(addressReq)
-                +'&key=AIzaSyAFbHBKFi2jgpifwJcilAgDE99QBofYGGA';
 
     request({
-                headers : {'Content-Type' : 'application/json'},  
-                uri : url,
-                method : 'GET'
-            },
-            function (err, response, body){
-                
-                if(err){
-                    return console.log(err);
-                }
-                var parsedJSON = JSON.parse(response.body.replace(/\r?\n|\r/g, ''));
-                
-                res.json(parsedJSON.results[0].geometry.location);
+        headers: { 'Content-Type': 'application/json' },
+        url: 'https://maps.googleapis.com/maps/api/geocode/json?&address='
+            + encodeURIComponent(addressReq)
+            + '&key=AIzaSyAFbHBKFi2jgpifwJcilAgDE99QBofYGGA',
+        method: 'GET'
+    },
+        function (err, response, body) {
+
+            if (err) {
+                return console.log(err);
             }
+            var parsedJSON = JSON.parse(response.body.replace(/\r?\n|\r/g, ''));
+
+            res.json(parsedJSON.results[0].geometry.location);
+        }
     );
 };
 
 exports.get_top_fifty_nearly_address = function (req, res) {
 
-    
+
 };
 
 exports.get_complete_address = function (req, res) {
-    Message.find({}, function (err, msg) {
-        if (err)
-            res.send(err);
-        res.json(msg);
-    });
+
+    var cep = req.body.cep;
+
+    //1o request: recuperar dados a partir do cep
+    request({
+        headers: { 'Content-Type': 'application/json' },
+        uri: 'https://maps.googleapis.com/maps/api/geocode/json?&address='
+            + encodeURIComponent(cep)
+            + '&key=AIzaSyAFbHBKFi2jgpifwJcilAgDE99QBofYGGA',
+        method: 'GET'
+    },
+        function (err, response, body) {
+            if (err) {
+                return console.log(err);
+            }
+            var parsedJSON = JSON.parse(response.body.replace(/\r?\n|\r/g, ''));
+            var latlng = "" + parsedJSON.results[0].geometry.location.lat + "," + parsedJSON.results[0].geometry.location.lng;
+
+            //necessário fazer um 2o request passando latitude e longitude pois o response anterior não traz a rua
+            request({
+                headers: { 'Content-Type': 'application/json' },
+                uri: 'https://maps.googleapis.com/maps/api/geocode/json?&latlng='
+                    + encodeURIComponent(latlng)
+                    + '&key=AIzaSyAFbHBKFi2jgpifwJcilAgDE99QBofYGGA',
+                method: 'GET'
+            },
+                function (err, response, body) {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    var parsedJSON = JSON.parse(response.body.replace(/\r?\n|\r/g, ''));
+
+                    var address = {
+                        "district": parsedJSON.results[0].address_components[2].short_name,
+                            "city": parsedJSON.results[0].address_components[3].short_name,
+                           "state": parsedJSON.results[0].address_components[4].short_name,
+                         "country": parsedJSON.results[0].address_components[5].long_name,
+                          "street": parsedJSON.results[0].address_components[1].long_name,
+                    }
+
+                    res.send(address);
+                }
+            );
+        }
+    );
 };
