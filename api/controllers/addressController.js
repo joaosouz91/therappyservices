74b8d5e548c867ffe5bb7a234b8fec133b9a9455
Joao Victor <joaovictor.souz@gmail.com>
2019-03-17 23:35:44 -0300

    final adjust

--- api/controllers/addressController.js
+++ api/controllers/addressController.js
@@ -6,9 +6,9 @@ const googleMapsClient = require('@google/maps').createClient({
     key : 'AIzaSyAFbHBKFi2jgpifwJcilAgDE99QBofYGGA'
 });
 
-exports.get_latitude_longitude = function (req, res) {
+exports.get_location = function (req, res) {
 
-    var addressReq = req.body.address;
+    var addressReq = req.params.address;
 
     request({
         headers: { 'Content-Type': 'application/json' },
@@ -20,9 +20,15 @@ exports.get_latitude_longitude = function (req, res) {
         function (err, response, body) {
 
             if (err) {
-                return console.log(err);
+                res.status(400);
+                res.json({"erro" : "parametro incorreto" });
             }
+            //parse
             var parsedJSON = JSON.parse(response.body.replace(/\r?\n|\r/g, ''));
+            if(parsedJSON.status == "ZERO_RESULTS"){
+                res.status(400);
+                res.json({"erro" : "parametro incorreto" });
+            }
 
             res.json(parsedJSON.results[0].geometry.location);
         }
@@ -32,7 +38,6 @@ exports.get_latitude_longitude = function (req, res) {
 exports.get_distance = function (req, res) {
 
     //console.log(req.body.origin);
-
     var origin = req.body.origin;
     var destinations = req.body.destinations;
     var mode = req.body.mode;
@@ -69,7 +74,15 @@ exports.get_distance = function (req, res) {
 
 exports.get_complete_address = function (req, res) {
 
-    var cep = req.body.cep;
+    if(req.params.cep.length < 8 || req.params.cep.length > 9){
+        res.status(400);
+        return res.json({"erro" : "parametro incorreto" });
+    }
+
+    var cep = req.params.cep;
+    if(!cep.includes("-")){
+        cep = cep.slice(0, 5) + "-" + cep.slice(5);
+    }
 
     //1o request: recuperar dados a partir do cep
     request({
@@ -81,11 +94,17 @@ exports.get_complete_address = function (req, res) {
     },
         function (err, response, body) {
             if (err) {
-                return console.log(err);
+                res.status(400);
+                res.json({"erro" : "parametro incorreto" });
             }
+            //parse
             var parsedJSON = JSON.parse(response.body.replace(/\r?\n|\r/g, ''));
-            var latlng = "" + parsedJSON.results[0].geometry.location.lat + "," + parsedJSON.results[0].geometry.location.lng;
+            if(parsedJSON.status == "ZERO_RESULTS"){
+                res.status(400);
+                res.json({"erro" : "parametro incorreto" });
+            }
 
+            var latlng = "" + parsedJSON.results[0].geometry.location.lat + "," + parsedJSON.results[0].geometry.location.lng;
             //necessário fazer um 2º request passando latitude e longitude pois o response anterior não traz a rua
             request({
                 headers: { 'Content-Type': 'application/json' },
@@ -96,18 +115,23 @@ exports.get_complete_address = function (req, res) {
             },
                 function (err, response, body) {
                     if (err) {
-                        return console.log(err);
+                        res.status(400);
+                        res.json({"erro" : "parametro incorreto" });
                     }
+                    //parse
                     var parsedJSON = JSON.parse(response.body.replace(/\r?\n|\r/g, ''));
+                    if(parsedJSON.status == "ZERO_RESULTS"){
+                        res.status(400);
+                        res.json({"erro" : "parametro incorreto" });
+                    }
 
                     var address = {
                         "district": parsedJSON.results[0].address_components[2].short_name,
                             "city": parsedJSON.results[0].address_components[3].short_name,
-                           "state": parsedJSON.results[0].address_components[4].short_name,
-                         "country": parsedJSON.results[0].address_components[5].long_name,
-                          "street": parsedJSON.results[0].address_components[1].long_name,
+                        "state": parsedJSON.results[0].address_components[4].short_name,
+                        "country": parsedJSON.results[0].address_components[5].long_name,
+                        "street": parsedJSON.results[0].address_components[1].long_name,
                     }
-
                     res.send(address);
                 }
             );
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,20 +4,19 @@ module.exports = function (app) {
     var address = require('../controllers/addressController.js');
     
     // messages Routes
-    /*app.route('/messages')
+    app.route('/messages')
         .get(messages.list_all_messages)
         .post(messages.create_a_message);
     app.route('/messages/:msgId')
         .get(messages.read_a_message)
         .put(messages.update_a_message)
         .delete(messages.delete_a_message);
-    */
-   
+    
     // address Routes
-    app.route('/address/latitudeLongitude')
-        .get(address.get_latitude_longitude);
+    app.route('/address/location/:address')
+        .get(address.get_location);
     app.route('/address/distance')
         .get(address.get_distance);
-    app.route('/address/complete')
+    app.route('/address/complete/:cep')
         .get(address.get_complete_address);
 };
